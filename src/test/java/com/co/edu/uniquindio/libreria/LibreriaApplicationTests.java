1e25d60a6786bc6230d2907fdcc921ce98fbd23c
FelipePedraza <davidf.pedrazab@uqvirtual.edu.co>
2025-09-01 15:23:36 -0500

    creacion del backend

---
+++ src/test/java/com/co/edu/uniquindio/libreria/LibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.edu.uniquindio.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
