2fed7c0e23697c0134964917cff6532288c6028c
David Alfonso Posso Cano <dapossoc@uqvirtual.edu.co>
2025-09-02 10:03:30 -0500

    Fundionalidades 2 y 4 completadas al 100%

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,8 @@ INSERT INTO book (isbn, title, author) VALUES ('978-0743273565', 'The Hitchhiker
 INSERT INTO book (isbn, title, author) VALUES ('978-1400033416', '1984', 'George Orwell');
 INSERT INTO book (isbn, title, author) VALUES ('978-0061120084', 'Brave New World', 'Aldous Huxley');
 INSERT INTO book (isbn, title, author) VALUES ('978-0743273566', 'Dune', 'Frank Herbert');
+INSERT INTO book (isbn, title, author) VALUES ('978-0470647691', 'Calculus', 'James Stewart');
+INSERT INTO book (isbn, title, author) VALUES ('978-0201714111', 'Calculus', 'Michael Spivak');
+INSERT INTO book (isbn, title, author) VALUES ('978-0201029185', 'Classical Mechanics', 'Herbert Goldstein');
+INSERT INTO book (isbn, title, author) VALUES ('978-0465025275', 'The Feynman Lectures on Physics', 'Richard P. Feynman');
+INSERT INTO book (isbn, title, author) VALUES ('978-0321625920', 'University Physics', 'Hugh D. Young');
